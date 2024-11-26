79c2b60689479b273cddf92a1b5f101577eee6d4
samhigg <samhigg@iu.edu>
2024-11-26 17:54:38 -0500

    create nav and add tommy font

---
+++ src/components/nav/nav.js
@@ -0,0 +1,42 @@
+"use client";
+import navStyles from "./nav.module.css";
+import React, { useState } from 'react';
+
+export default function Nav () {
+    
+    const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+    
+    const mouseHandler = (event) => {
+        setMousePos({ x: event.clientX, y: event.clientY });
+        const relX = (100 * (event.clientX / window.innerWidth));
+        document.getElementById("logo").style.left = ((relX - 105) + "vw");
+    };
+
+    return (<div onMouseMove={mouseHandler}>
+        <nav className={navStyles.nav}>
+            <div className={navStyles.logo} id="logo">
+                <div className={navStyles.outBox}>
+                    <div className={navStyles.inBox}></div>
+                    <div className={navStyles.inBox}></div>
+                    <div className={navStyles.inBox}></div>
+                </div>
+                <img src="curlingStone.svg" alt="" />
+            </div>
+            <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/1">1</a>
+            </li>
+            <li>
+                <a href="/2">2</a>
+            </li>
+            <li>
+                <a href="/3">3</a>
+            </li>
+            </ul>
+        </nav>
+        <div className={navStyles.navGapper}></div>
+      </div>)
+}
