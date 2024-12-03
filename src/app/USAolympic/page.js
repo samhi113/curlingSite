564f15a6022c07120a6a218bfe513055e80c3cb0
arigree <arigree@iu.edu>
2024-12-03 13:00:14 -0500

    Initializing pages: USA Olympic Teams, Science Behind Curling, Iconic Players, and their respective CSS files.

---
+++ src/app/USAolympic/page.js
@@ -0,0 +1,9 @@
+import * as pageStyles from './usa.module.css';
+
+export default function USA(){
+    return(
+        <main>
+            <h1>USA Olympic Teams</h1>
+        </main>
+    )
+}
---
+++ src/app/science/page.js
@@ -0,0 +1,9 @@
+import * as pageStyles from './science.module.css';
+
+export default function science(){
+    return(
+        <main>
+            <h1>The Science Behind Curling</h1>
+        </main>
+    )
+}
