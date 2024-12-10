f1af2b18fa9726c784414f75695a9aacc0082c7f
arigree <arigree@iu.edu>
2024-12-09 20:36:34 -0500

    Setting up science behind curling page. header image some styling

--- src/app/science/page.js
+++ src/app/science/page.js
@@ -2,8 +2,13 @@ import * as pageStyles from './science.module.css';
 
 export default function science(){
     return(
-        <main>
-            <h1>The Science Behind Curling</h1>
+        <main className={pageStyles.main}>
+            <header className={pageStyles.mainHeader}>
+               <h1 className={pageStyles.headerText}>The Science Behind Curling</h1> 
+            </header>
+            <article className={pageStyles.container}>
+                
+            </article>
         </main>
     )
 }
