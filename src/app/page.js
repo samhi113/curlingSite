c359cbc2f833937c226aaeb2de4f86918512b7a2
Salmane Issa <143265121+Sgissa@users.noreply.github.com>
2024-12-11 02:21:39 +0000

    Home page created

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
+"use client"
 
-"use client";
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
 export default function Home() {
   console.log("a")
   return (
-    <main className={styles.main}>
-      
+    <main>
+      <div className={styles.hero}>
+            <h1 className={styles.title}> 
+                Curling
+            </h1>
+            </div> 
+            <section className={styles.container}>
+                <p className={styles.subTitle}>This Site Is All About Curling</p>
+                <div className={styles.content}>
+                  <img className={styles.pic} src='./todd.png'/>
+                  <p className={styles.ptext}>One man shined a light on the sport of curling like no other. In honor of Proffessor Todd, We have made a dedicated website to the sport of curling.</p>
+                </div>
+            </section>
     </main>
-  );
-}
+  )
+};
+
